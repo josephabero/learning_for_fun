880744ba4836bd3796f689dff8a93f6cc9fe60e7
Joseph Abero <joseph.abero@yahoo.com>
2020-07-04 01:27:37 -0700

    Generate file with script, template, and json file

--- jinja/scripts/generate_test.py
+++ jinja/scripts/generate_test.py
@@ -1,22 +1,21 @@
-from helper_functions import get_jinja_environment, write_to_file
+from helper_functions import get_jinja_environment, write_to_file, read_json_file
 import json
+import os
 
-ROOT = "/mnt/c/cmpe/jinja"
+ROOT = "/mnt/c/cmpe/learning_for_fun/jinja"
 TEMPLATES_FOLDER = ROOT + "/templates"
 TEMPLATE = "test.jinja"
 GENERATED_FILE = ROOT + "/generated/test.txt"
 SOURCE_FILE = ROOT + "/source/friends.json"
 
-
-
 def main():
-    # jinja_environment = get_jinja_environment(TEMPLATES_FOLDER)
-    # template = jinja_environment.get_template(TEMPLATE)
-    # render = template.render(test=friends)
-    # write_to_file(render, GENERATED_FILE)
-    with open(SOURCE_FILE, "r") as file:
-        content = json.load(file)
-        print(json.dumps(content, indent=4))
+    jinja_environment = get_jinja_environment(TEMPLATES_FOLDER)
+    template = jinja_environment.get_template(TEMPLATE)
+    content = read_json_file(SOURCE_FILE)
+    print(content)
+    render = template.render(test=content)
+    write_to_file(render, GENERATED_FILE)
+    
 
 
 if __name__ == "__main__":
--- jinja/scripts/helper_functions.py
+++ jinja/scripts/helper_functions.py
@@ -1,11 +1,16 @@
 import jinja2
+import json
 
 def get_jinja_environment(template):
-	file_loader = jinja2.FileSystemLoader(searchpath=template)
-	return jinja2.Environment(loader=file_loader)
+    file_loader = jinja2.FileSystemLoader(searchpath=template)
+    return jinja2.Environment(loader=file_loader)
 
 def write_to_file(content, output_file):
-	""" Writes string to file
-	"""
-	with open(output_file, 'w') as file:
-		file.write(content)
+    """ Writes string to file
+    """
+    with open(output_file, 'w') as file:
+        file.write(content)
+
+def read_json_file(json_file):
+    with open(json_file, "r") as file:
+        return json.load(file)
